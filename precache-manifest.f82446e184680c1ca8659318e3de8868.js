8f483569aadebbf28c498ceaa9dd8b9c44ae1633
Dafydd <dafyddzephyr@gmail.com>
2019-03-28 00:22:33 -0700

    Updates

---
+++ precache-manifest.f82446e184680c1ca8659318e3de8868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "95f99af3c990ce692ad9",
+    "url": "/Memory-Game/static/js/main.0869984f.chunk.js"
+  },
+  {
+    "revision": "ce84c4e0833e11ff4cf6",
+    "url": "/Memory-Game/static/js/2.5eb39cc8.chunk.js"
+  },
+  {
+    "revision": "95f99af3c990ce692ad9",
+    "url": "/Memory-Game/static/css/main.fb78f318.chunk.css"
+  },
+  {
+    "revision": "1fd17d4c7a63dc90be20d5918eae889e",
+    "url": "/Memory-Game/index.html"
+  }
+];
